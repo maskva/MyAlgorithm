c1c828df930c3591170433dc73103379b3c30e05
maskva <44914344+maskva@users.noreply.github.com>
2022-07-08 16:00:42 +0800

    Update ptree.cpp
    将pivot=null时的concat()用另一个函数重载
    完善了copy()，复制后，lc,rc应置为null
    自此，项目基本完成

--- ptree.cpp
+++ ptree.cpp
@@ -35,10 +35,16 @@ struct Node {
 };
 
 int rho(ValueType val1, ValueType val2) { return val2; }
+bool function(Node* t) {
+    return t->key % 3 == 0;
+}
 
 //复制节点
 Node* copy(Node* t) {
-    return  new Node(*t);
+    Node* temp = new Node(*t);
+    temp->lc = nullptr;
+    temp->rc = nullptr;
+    return  temp;
 }
 //返回子树节点数目 Morris先序
 int size(Node* t) {
@@ -216,87 +222,108 @@ int binarySearch(KeyType A[], int m, KeyType targetkey) {
     return left;
 }
 
-//连接父子节点  done
-Node* concat(Node* L, Node* t, Node* R) {
-    if (t != nullptr) {
-        if (getHeight(L) > getHeight(R) + 1) {
-            t->rc = R;
-            Node* p = L;
-            Node* pre_to_p = nullptr;
-            //寻找高为h(R)+1的子树，根节点为p->rc,
-            //特殊情况：h(L->l)=h(R)+1,h(L->r)=h(R),此时，该子树的高度为h(R)
-            while (getHeight(p->rc) > getHeight(R) + 1) {
-                pre_to_p = p;
-                p = p->rc;
-            }
-            t->lc = p->rc;
-            p->rc = t;
-            t->height = max(getHeight(t->lc), getHeight(t->rc)) + 1;
-            //p的左右子树高度分别为h(R),h(R)+1
-            if (getHeight(p->lc) < getHeight(t->lc)) {
-                p->height = max(getHeight(p->lc), getHeight(p->rc)) + 1;
-                if (pre_to_p == nullptr) {//也即p=L，进行RL旋转后，根节点会变
-                    return RL_rotation(p);
-                }
-                else {
-                    pre_to_p->rc = RL_rotation(p);
-                }
-            }
-            //p的左右子树高度都为h(R)+1,(都为h(R)的情况不存在,原因见前面的注释)
-            //此时应当更新从root到p路径上节点的height
-            else if (getHeight(p->lc) == getHeight(t->lc)) {
-                p->height = max(getHeight(p->lc), getHeight(p->rc)) + 1;
-                updateHeight(L, p, true);
-            }
-            updateHash(L, p, true);
-            return L;
-        }
-        else if (getHeight(R) > getHeight(L) + 1) {
-            t->rc = L;
-            Node* p = R;
-            Node* pre_to_p = nullptr;
-            while (getHeight(p->lc) > getHeight(L) + 1) {
-                pre_to_p = p;
-                p = p->lc;
-            }
-            t->rc = p->lc;
-            p->lc = t;
-            t->height = max(getHeight(t->lc), getHeight(t->rc)) + 1;
-            if (getHeight(p->rc) < getHeight(t->rc)) {
-                p->height = max(getHeight(p->lc), getHeight(p->rc)) + 1;
-                if (pre_to_p == nullptr) {
-                    return LR_rotation(p);
-                }
-                else {
-                    pre_to_p->lc = LR_rotation(p);
-                }
-            }
-            else if (getHeight(p->rc) == getHeight(t->rc)) {
-                p->height = max(getHeight(p->lc), getHeight(p->rc)) + 1;
-                updateHeight(R, p, false);
-            }
-            updateHash(R, p, false);
-            return R;
 
+//连接  done
+Node* concat(Node* L, Node* t, Node* R) {//上层调用的函数保证了t!=null
+
+    if (getHeight(L) > getHeight(R) + 1) {//若R==null,处理方式一致，不必单独处理
+        t->rc = R;
+        Node* p = L;
+        Node* pre_to_p = nullptr;
+        //寻找高为h(R)+1的子树，根节点为p->rc,
+        //特殊情况：h(L->l)=h(R)+1,h(L->r)=h(R),此时，该子树的高度为h(R)
+        while (getHeight(p->rc) > getHeight(R) + 1) {
+            pre_to_p = p;
+            p = p->rc;
         }
-        else {
-            t->lc = L;
-            t->rc = R;
-            t->height = max(getHeight(L), getHeight(R)) + 1;
-            t->hash = calculateHash(getHash(L), getHash(R));
-            return t;
+        t->lc = p->rc;
+        p->rc = t;
+        t->height = max(getHeight(t->lc), getHeight(t->rc)) + 1;
+        //p的左右子树高度分别为h(R),h(R)+1
+        if (getHeight(p->lc) < getHeight(t->lc)) {
+            p->height = max(getHeight(p->lc), getHeight(p->rc)) + 1;
+            if (pre_to_p == nullptr) {//也即p=L，进行RL旋转后，根节点会变
+                return RL_rotation(p);
+            }
+            else {
+                pre_to_p->rc = RL_rotation(p);
+            }
         }
+        //p的左右子树高度都为h(R)+1,(都为h(R)的情况不存在,原因见前面的注释)
+        //此时应当更新从root到p路径上节点的height
+        else if (getHeight(p->lc) == getHeight(t->lc)) {
+            p->height = max(getHeight(p->lc), getHeight(p->rc)) + 1;
+            updateHeight(L, p, true);
+        }
+        updateHash(L, p, true);
+        return L;
+    }
+    else if (getHeight(R) > getHeight(L) + 1) {
+        t->rc = L;
+        Node* p = R;
+        Node* pre_to_p = nullptr;
+        while (getHeight(p->lc) > getHeight(L) + 1) {
+            pre_to_p = p;
+            p = p->lc;
+        }
+        t->rc = p->lc;
+        p->lc = t;
+        t->height = max(getHeight(t->lc), getHeight(t->rc)) + 1;
+        if (getHeight(p->rc) < getHeight(t->rc)) {
+            p->height = max(getHeight(p->lc), getHeight(p->rc)) + 1;
+            if (pre_to_p == nullptr) {
+                return LR_rotation(p);
+            }
+            else {
+                pre_to_p->lc = LR_rotation(p);
+            }
+        }
+        else if (getHeight(p->rc) == getHeight(t->rc)) {
+            p->height = max(getHeight(p->lc), getHeight(p->rc)) + 1;
+            updateHeight(R, p, false);
+        }
+        updateHash(R, p, false);
+        return R;
+
     }
     else {
-        //to do
-        return nullptr;
+        t->lc = L;
+        t->rc = R;
+        t->height = max(getHeight(L), getHeight(R)) + 1;
+        t->hash = calculateHash(getHash(L), getHash(R));
+        return t;
     }
-
 }
+
+//找到树的最小值节点（最右下）,并递归调整，保持平衡
+Node* splitLast(Node* t, Node*& k) {
+    if (t->rc == nullptr) {//上层调用的函数保证了t!=null
+        Node* L = t->lc;
+        k = t;
+        k->lc = nullptr;
+        k->height = 1;
+        return L;
+    }
+    else
+    {
+        Node* temp = splitLast(t->rc, k);
+        return concat(t->lc, t, temp);
+    }
+}
+
+//连接
+Node* concat(Node* L, Node* R) {
+    if (L == nullptr)return R;
+    else if (R == nullptr)return L;
+    Node* k = nullptr;
+    L = splitLast(L, k);
+    return concat(L, k, R);
+}
+
 
 //引用计数器加1
 void add_ref(Node* t) {
-    if(t!= nullptr){
+    if (t != nullptr) {
         t->reference_counter++;
     }
 }
@@ -304,18 +331,24 @@ void add_ref(Node* t) {
 //返回所有>=k的节点构成的树
 Node* right(Node* t, KeyType k) {
     if (t == nullptr)return nullptr;
-    if (t->key<k)return right(t->rc, k);
+    if (t->key < k)return right(t->rc, k);
     Node* t2 = copy(t);
     add_ref(t->rc);
-    return concat(right(t->lc, k), t2, t->rc);
+    //return concat(right(t->lc, k), t2, t->rc);
+    t2->lc = right(t->lc, k);
+   
+    return t2;
 }
 //返回所有<=k的节点构成的树
 Node* left(Node* t, KeyType k) {
     if (t == nullptr)return nullptr;
-    if (t->key>k)return left(t->lc, k);
+    if (t->key > k)return left(t->lc, k);
     Node* t2 = copy(t);
     add_ref(t->lc);
-    return concat(t->lc,t2,left(t->rc, k));
+    //return concat(t->lc, t2, left(t->rc, k));
+   
+    t2->rc = left(t->rc, k);
+    return t2;
 }
 
 //t:根节点;A[]:待插入的元组(已排序);m:元组个数
@@ -344,18 +377,22 @@ Node* range(Node* t, KeyType kl, KeyType kr) {
     Node* L = right(cur->lc, kl);
     Node* R = left(cur->rc, kr);
     Node* mid = copy(cur);
-    return concat(L, mid, R);
+    
+    //return concat(L, mid, R);
+    mid->lc = L;
+    mid->rc = R;
+    return mid;
 }
 
 Node* filter(Node* t, bool (*function)(Node* t)) {
     if (t == nullptr)return nullptr;
     Node* L = filter(t->lc, function);
     Node* R = filter(t->rc, function);
-    if (function) {
+    if (function(t)) {
         return concat(L, copy(t), R);
     }
     else {
-        return concat(L, nullptr, R);
+        return concat(L, R);
     }
 }
 
@@ -434,6 +471,7 @@ void test_multi_insert() {
     //cout << endl;
     Node* indexRoot2 = multi_insert(indexRoot, tuples2, m2, rho);
     travel(indexRoot2); cout << endl;
+    travel(indexRoot); cout << endl;
 }
 
 //done
@@ -485,6 +523,39 @@ void test_height(int const m) {
 }
 
 //done
+void test_concat_without_pivot() {
+    int const m1 = 10, m2 = 3;
+    Tuple tuples1[m1], tuples2[m2];
+    cout << "build p-tree" << endl;
+
+    KeyType build_key1[] = { 1,2,3,4,5,6,7,8,9,10, };
+    ValueType build_value1[] = { 1,1,1,1,1,1,1,1,1,1 };
+    for (int i = 0; i < m1; i++) {
+        tuples1[i].key = build_key1[i];
+        tuples1[i].val = build_value1[i];     
+    }
+
+    KeyType build_key2[] = { 17,18,19 };
+    ValueType build_value2[] = { 3,3,3 };
+    for (int i = 0; i < m2; i++) {
+        tuples2[i].key = build_key2[i];
+        tuples2[i].val = build_value2[i];     
+    }
+  
+    Node* indexRoot1 = nullptr;
+    Node* indexRoot2 = nullptr;
+    Node* indexRoot3 = nullptr;
+
+    indexRoot1 = multi_insert(indexRoot1, tuples1, m1, rho);
+    indexRoot2 = multi_insert(indexRoot2, tuples2, m2, rho);
+    travel(indexRoot1); cout << endl;
+    travel(indexRoot2); cout << endl;
+    indexRoot3 = concat(indexRoot1, indexRoot2);
+    travel(indexRoot3); cout << endl;
+    
+}
+
+//done
 void test_concat() {
     int const m1 = 15, m2 = 3;
     Tuple tuples1[m1], tuples2[m2];
@@ -521,6 +592,7 @@ void test_concat() {
     cout << endl;
 }
 
+
 //done
 void tesy_left_right() {
     cout << "build p-tree" << endl;
@@ -534,9 +606,9 @@ void tesy_left_right() {
         tuples[i].val = build_value[i];
     }
     Node* indexRoot = nullptr;
-    indexRoot = multi_insert(indexRoot, tuples, m, rho);travel(indexRoot); cout<<endl;
-    Node* indexRoot2= right(indexRoot->lc,2); travel(indexRoot2);cout<<endl;
-    Node* indexRoot3= left(indexRoot->rc,14);travel(indexRoot3);cout<<endl;
+    indexRoot = multi_insert(indexRoot, tuples, m, rho); travel(indexRoot); cout << endl;
+    Node* indexRoot2 = right(indexRoot->lc, 2); travel(indexRoot2); cout << endl;
+    Node* indexRoot3 = left(indexRoot->rc, 14); travel(indexRoot3); cout << endl;
 
 }
 
@@ -547,8 +619,6 @@ void test_range() {
     Tuple tuples[m];
     KeyType build_key[] = { 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18 };
     ValueType build_value[] = { 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 };
-    //KeyType build_key[] = {1,2,3,4,5,6,7};
-    //ValueType build_value[] = { 0,0,0,0,0,0,0 };
     for (int i = 0; i < m; i++) {
         tuples[i].key = build_key[i];
         tuples[i].val = build_value[i];
@@ -558,12 +628,31 @@ void test_range() {
     travel(indexRoot);
     cout << endl;
     cout << "test_range " << endl;
-    Node* indexRoot2 = range(indexRoot, 2, 11);
-    travel(indexRoot2);
+    Node* indexRoot2 = range(indexRoot, 4, 12);
+    //travel(indexRoot2);
     cout << endl;
+    cout << endl;
+    travel(indexRoot);
+   
 }
-//to do
+
+//done
 void test_fileter() {
+    cout << "build p-tree" << endl;
+    int const m = 18;
+    Tuple tuples[m];
+    KeyType build_key[] = { 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18 };
+    ValueType build_value[] = { 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 };
+    for (int i = 0; i < m; i++) {
+        tuples[i].key = build_key[i];
+        tuples[i].val = build_value[i];
+    }
+    Node* indexRoot = nullptr;
+    indexRoot = multi_insert(indexRoot, tuples, m, rho);
+    travel(indexRoot); cout << endl;
+
+    Node* indexRoot2 = filter(indexRoot,function);
+    travel(indexRoot2); cout << endl;
 
 }
 //to do
@@ -589,8 +678,31 @@ void test_avl() {
     travel(root); cout << endl;
 }
 
+//done 
+void test_splitLast() {
+    Node* indexRoot = nullptr;
+    //build----------------------------------------------------
+    cout << "build p-tree" << endl;
+    int const m = 10;
+    Tuple tuples[m];
+    KeyType build_key[] = { 1,3,4,5,9,11,13,15,17,19};
+    ValueType build_value[] = { 0,0,0,0,0,0,0,0,0};
+    for (int i = 0; i < m; i++) {
+        tuples[i].key = build_key[i];
+        tuples[i].val = build_value[i];    
+    } 
+    indexRoot = multi_insert(indexRoot, tuples, m, rho);
+    travel(indexRoot); cout << endl;
+
+    Node* k;
+    Node* indexRoot2 = splitLast(indexRoot, k);
+    travel(indexRoot2); cout << endl;
+    Node* indexRoot3 = splitLast(indexRoot2, k);
+    travel(indexRoot3); cout << endl;
+}
+
 int main()
 {
-    test_range();
+    test_fileter();
     return 0;
 }
